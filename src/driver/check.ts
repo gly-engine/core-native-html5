08d1651ee50a4df0d024c613cd1d626c9336678b
RodrigoDornelles <rodrigo@dornelles.me>
2025-03-20 16:51:36 -0300

    feat: improve compatibility

--- src/driver/check.ts
+++ src/driver/check.ts
@@ -1,25 +1,25 @@
 const mock = async (_: {}) => {}
 
-async function wasmoon(hv: {lua: unknown}) {
+async function check_wasmoon(hv: {lua: unknown}) {
     if (!hv.lua) {
         throw new Error('wasmoon is required!')
     }
 }
 
-async function fengari(hv: {fengari: unknown}) {
-    if (!hv.fengari) {
+async function check_fengari(hv: {vm: {fengari: unknown}}) {
+    if (!hv.vm.fengari) {
         throw new Error('fengari is required!')
     }
 }
 
 export default {
     wasmoon: {
-        prepare: wasmoon,
+        prepare: check_wasmoon,
         install: mock,
         startup: mock
     },
     fengari: {
-        prepare: fengari,
+        prepare: check_fengari,
         install: mock,
         startup: mock
     },
@@ -27,7 +27,7 @@ export default {
         prepare: async (hv) => {
             let errors = 0;
 
-            await Promise.allSettled([fengari(hv), wasmoon(hv)]).then(results => {
+            await Promise.allSettled([check_wasmoon(hv), check_fengari(hv)]).then(results => {
                 errors = results.filter(result => result.status === "rejected").length
             })
 

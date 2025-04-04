117fca1f43dc877c62cdcf377e60f2ad02dfaecd
rodrigodornelles <rodrigo.dornelles.poa@gmail.com>
2025-04-04 09:25:21 -0300

    fix: print number 0 in native_draw_text

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -55,9 +55,9 @@ export function create_backend(canvas: HTMLCanvasElement, ctx: CanvasRenderingCo
         // for legacy compatibility:
         native_draw_poly: (mode: number, verts: Array<number>, x = 0, y = 0, scale = 1, angle = 0, ox = 0, oy = 0) =>  backend_canvas.native_draw_poly2(render, mode, verts, x, y, scale, angle, ox, oy),
         native_draw_image: (src: string, x: number, y: number) => backend_image.native_image_draw(render, image_cache, src, x, y),
-        native_draw_text: (x: number | string, y: number, text: string) => {
-            typeof x == 'number' && text && backend_text.native_text_print(render, text_cache, x, y, text)        
-            return backend_text.native_text_mensure(render, text_cache, text ?? x)
+        native_draw_text: (x: number | string, y: number, text: string | number) => {
+            typeof x == 'number' && (text || text == 0) && backend_text.native_text_print(render, text_cache, x, y, `${text}`)        
+            return backend_text.native_text_mensure(render, text_cache, `${text ?? x}`)
         },
         native_draw_font: (name: string, size: number) => {
             backend_text.native_text_font_name(render, text_cache, name)

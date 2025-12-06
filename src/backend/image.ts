22cf6120a05f7eb621f533787978ec416108df0c
RodrigoDornelles <rodrigo.dornelles.poa@gmail.com>
2025-12-06 20:21:15 -0300

    feat: improve image api to 0.3.X

--- src/backend/image.ts
+++ src/backend/image.ts
@@ -1,46 +1,104 @@
 import { renderI, imageD } from "../type";
 
-function preload_image(src) {
+function preload_image(src: string) {
     return document.querySelector(`img[src="${src}"]`) as HTMLImageElement | null
 }
 
 async function load_image(src: string) {
-    return new Promise<HTMLImageElement>((resolve, reject) => {
-        const error = new Error(`img ${src} error!`)
+    return new Promise<HTMLImageElement | null>((resolve, reject) => {
         const el = document.createElement('img')
         el.src = src
-        el.onload = () => resolve(el), 
-        el.onerror = () => reject(error)
-        el.onabort = () => reject(error)
+        const failed = (ev: string | Event) => {
+            resolve(null)
+            console.error(`[core:html5] image src "${src}" ${typeof ev === 'string'? ev: ev.type}`)
+        }
+        el.onerror = failed
+        el.onabort = failed
+        el.onload = () => resolve(el)
     });
 }
 
-export function native_image_load(render: renderI, cache: imageD, src: string, url?: string) {
-    if (!(src in cache)) {
-        cache[src] = preload_image(url ?? src)
-        if (!cache[src]) {
-            load_image(src).then((el) => cache[src] = el)
+function get_image_from_id(cache: imageD, id: number | undefined) {
+    if (id) {
+        const el = cache.data[id]
+        if (el === null) {
+            return undefined
         }
+        if (el === undefined) {
+            throw new Error(`[core:html5] image id ${id} not exist!`)
+        }
+        return el
+    }
+    return undefined
+}
+
+export function native_image_load(render: renderI, cache: imageD, src: string, url?: string) {
+    const id = cache.name[src]
+
+    if (id) {
+        return cache.data[id]? id: undefined
+    }
+
+    const el_preloaded = preload_image(src)
+
+    if (el_preloaded) {
+        cache.name[src] = ++cache.count
+        return cache.count
+    }
+
+    const new_id = ++cache.count
+    cache.name[src] = new_id
+    load_image(src).then((el) => cache.data[new_id] = el)
+
+    return undefined
+}
+
+export function native_image_draw(render: renderI, cache: imageD, src: string | number, x: number, y: number) {
+    const id = typeof src == 'string'? native_image_load(render, cache, src): src;
+    const el = get_image_from_id(cache, id)
+    
+    if (el) {
+        render.ctx.drawImage(el, x, y)
     }
 }
 
-export function native_image_draw(render: renderI, cache: imageD, src: string, x: number, y: number) {
-    native_image_load(render, cache, src)
-    if (cache[src]) {
-        render.ctx.drawImage(cache[src], x, y)
+export function native_image_mensure(render: renderI, cache: imageD, src: string | number) {
+    const id = typeof src == 'string'? native_image_load(render, cache, src): src;
+    const el = get_image_from_id(cache, id)
+   
+    if (el) {
+        return [el.width, el.height]
     }
+
+    return [0, 0]
 }
 
-export function native_image_mensure(render: renderI, cache: imageD, src: string) {
-    native_image_load(render, cache, src)
-    if (cache[src]) {
-        return [cache[src].width, cache[src].height]
+export function native_image_unload(cache: imageD, src: string | number) {
+    const clear_by_name = (src: string) => {
+        do {
+            const id = cache.name[src]
+            if (!id) break;
+
+            delete cache.name[src];
+
+            if (!cache.data[id]) break;
+
+            delete cache.data[id];
+        }
+        while(0);
     }
-    return []
+
+
+    if (typeof src == 'number') {
+        const name = Object.entries(cache.name).find(([_, id]) => id == src)?.[0] ?? '';
+        return clear_by_name(name)
+    }
+
+    return clear_by_name(src)
 }
 
-export function native_image_clear_all(cache: imageD) {
-    for (const key in cache) {
-        delete cache[key]
-    }
+export function native_image_unload_all(cache: imageD) {
+    cache.count = 0
+    cache.data = []
+    cache.name = {}
 }
